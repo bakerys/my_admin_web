2c8a1b9154ac62a027c1c1a4d235822872d316e6
zaishushangchangge@163.com <1738286725@qq.com>
2024-04-03 18:00:56 +0800

    base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: "/my_admin_web/",
-  // base: "./",
+  base: "/my_admin_web/", // 关乎部署之后的打包文件路径
   plugins: [react(), WindiCSS()],
 });
