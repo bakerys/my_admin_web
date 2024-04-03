d0b89856f1bee02f768f34fa6502663da68db383
zaishushangchangge@163.com <1738286725@qq.com>
2024-04-03 16:36:11 +0800

    base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: "/my_admin_web/",
   base: "./",
   plugins: [react(), WindiCSS()],
 });
